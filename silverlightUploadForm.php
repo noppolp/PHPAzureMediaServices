bb7283862a3fb8bf3e157b1157726278ce01863f
Noppol Pilukruangdet <knookcs08@gmail.com>
2013-06-07 11:08:10 +0700

    silverlightuploadform

---
+++ silverlightUploadForm.php
@@ -0,0 +1,4 @@
+<form id="gbForm" action="silverlightUpload.php" method="post">
+	Asset Id: <input type="text" id="AssetIdTextBox" name="AssetIdTextBox" class="field" size="50"/>
+	<input type="submit" value="Upload" />
+</form>
